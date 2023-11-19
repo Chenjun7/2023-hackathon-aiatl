2c7356e03636113ab2651bab0154180422f8ad4b
Nikhel Krishna <nikhelkrishna@gmail.com>
2023-11-19 06:02:02 -0500

    FINAL COMMIT

--- GIS_Assistant_Again/flask-server/server.py
+++ GIS_Assistant_Again/flask-server/server.py
@@ -10,9 +10,8 @@ app = Flask(__name__)
 
 PROMPT = "Annotate the location of the building with the largest area using the buildings file"
 
-
-client = OpenAI(api_key='OPENAI_API_KEY_HERE')
-assistant = client.beta.assistants.retrieve("ASSISTANT_ID_HERE")
+client = OpenAI(api_key='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')
+assistant = client.beta.assistants.retrieve("asst_OjqT6XZVRqEKvmjukXBlecfd")
 
 thread = client.beta.threads.create()
 
@@ -41,59 +40,77 @@ def send_message():
         run_id=run.id
     )
     i += 1
-    if i == 20:
+    if i == 60:
+      run = client.beta.threads.runs.cancel(
+        thread_id=thread.id,
+        run_id=run.id
+      )
       return jsonify({'text_response': "Could not process request", 'annotation_data': None})
     print(run.status)
-  if run.status == "completed":
-    messages = client.beta.threads.messages.list(
-    thread_id=thread.id
-    )
-    print("map did not annottate")
-    print(messages)
-    
-  tools_to_call = run.required_action.submit_tool_outputs.tool_calls
-  tool_output_array = []
-  map_annotation_data = defaultdict(list)
-  numCoords = 0 
-  for tool_to_call in tools_to_call:
-    tool_call_id = tool_to_call.id
-    function_name = tool_to_call.function.name
-    function_arg = tool_to_call.function.arguments
-    print("Tool ID: " + tool_call_id)
-    print("Function to call: " + function_name)
-    print("Parameters to use: " + function_arg)
-    # TO DO call the API matching the functionname and return the output
-    if function_name == "get_map_coords":
-      numCoords += 1 
-      # Parse the JSON string into a Python dictionary
-      data = json.loads(function_arg)
-      # Extract the string containing the float values
-      float_values_str = data['list_of_points']
-      # Split the string by comma to separate the float values
-      print("bruhhhh")
-      print(type(data['building_names']))
-      points_list = ast.literal_eval(float_values_str)
-      name_list = ast.literal_eval(data['building_names'])
-      print(type(name_list))
-      if numCoords == 1:
-        if data.get('building_names') is not None:
-          map_annotation_data['building_name'] = name_list
-        map_annotation_data['list_of_points'] = points_list
-      else:
-        map_annotation_data['building_name'].append(name_list)
-        map_annotation_data['list_of_points'].append(points_list)
+  if run.status == "requires_action":    
+    tools_to_call = run.required_action.submit_tool_outputs.tool_calls
+    tool_output_array = []
+    map_annotation_data = defaultdict(list)
+    numCoords = 0 
+    for tool_to_call in tools_to_call:
+      tool_call_id = tool_to_call.id
+      function_name = tool_to_call.function.name
+      function_arg = tool_to_call.function.arguments
+      print("Tool ID: " + tool_call_id)
+      print("Function to call: " + function_name)
+      print("Parameters to use: " + function_arg)
+      # TO DO call the API matching the functionname and return the output
+      if function_name == "get_map_coords":
+        numCoords += 1 
+        # Parse the JSON string into a Python dictionary
+        data = json.loads(function_arg)
+        # Extract the string containing the float values
+        float_values_str = data['list_of_points']
+        # Split the string by comma to separate the float values
+        print("bruhhhh")
+        points_list = ast.literal_eval(float_values_str)
+        name_list = (data['building_names'])
+        if data['building_names'][0] == '[':
+          name_list = ast.literal_eval(data['building_names'])
+        print(type(name_list))
+        if numCoords == 1:
+          if data.get('building_names') is not None:
+            map_annotation_data['building_name'] = name_list
+          map_annotation_data['list_of_points'] = points_list
+        else:
+          map_annotation_data['building_name'].append(name_list)
+          map_annotation_data['list_of_points'].append(points_list)
 
-      # Convert the string values to float and then to int
-      print(points_list[0])
-
-
+        # Convert the string values to float and then to int
+        print(points_list[0])
+      if function_name == "get_map_polygons":
+        numCoords += 1 
+        # Parse the JSON string into a Python dictionary
+        data = json.loads(function_arg)
+        # Extract the string containing the float values
+        float_values_str = data['list_of_polygons']
+        # Split the string by comma to separate the float values
+        print("bruhhhh")
+        points_list = ast.literal_eval(float_values_str)
+        name_list = (data['name_description'])
+        if data['name_description'][0] == '[':
+          name_list = ast.literal_eval(data['name_description'])
+        print(type(name_list))
+        if numCoords == 1:
+          if data.get('name_description') is not None:
+            map_annotation_data['name_description'].append(name_list)
+          map_annotation_data['list_of_polygons'] = points_list
+        else:
+          map_annotation_data['name_description'].append(name_list)
+          map_annotation_data['list_of_polygons'].append(points_list)
+        
       output = True
-    tool_output_array.append({"tool_call_id": tool_call_id, "output":output})
-  run = client.beta.threads.runs.submit_tool_outputs(
-    thread_id=thread.id,
-    run_id=run.id,
-    tool_outputs=tool_output_array
-  )
+      tool_output_array.append({"tool_call_id": tool_call_id, "output":output})
+    run = client.beta.threads.runs.submit_tool_outputs(
+      thread_id=thread.id,
+      run_id=run.id,
+      tool_outputs=tool_output_array
+    )
   while run.status not in ["completed", "failed", "requires_action"]:
     time.sleep(1)
 
